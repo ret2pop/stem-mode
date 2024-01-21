80fd75054f568b079ab040a4ccbbfb532ba5e5dd
Preston Pan <preston@nullring.xyz>
2024-01-21 15:02:14 -0800

    sort of functional

--- stem-mode.el
+++ stem-mode.el
@@ -1,39 +1,58 @@
-;;; stem-mode.el -*- lexical-binding: t; -*-
+;;; stem-mode.el --- a major mode for the stem programming language                    -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2024 Preston Pan
 
+;; Author: Preston Pan <preston@nullring.xyz>
+;; Keywords: lisp stem
+;; Version: 0.0.1
+;; URL: https://stemdoc.nullring.xyz
+;; Package-Requires: ((emacs "24.3"))
+
+;;; Commentary:
+
+;; Provides syntax highlighting for stem.
+
+;;; Code:
 (defvar stem-constants
   '("E"
     "PI"))
 
 (defvar stem-builtins
-  '("def" "undef" "dip" "compose" "quote" "eval" "curry" "swap" "drop" "dsc" "cut" "read" "nop" "strquote" "exit"))
+  '("def" "undef" "vat" "eval" "curry" "cut" "nop" "strquote" "include" "read" "exit" "loop" "if" "when" "sort"))
 
 (defvar stem-functions
-  '("map" "filter" "reduce" "range" "loop" "abs" "neg" "factorial" "sin" "cos" "exp"))
+  '("map" "filter" "reduce" "range" "while" "keep" "abs" "neg" "factorial" "sin" "cos" "exp"))
+
+(defvar stem-stack-builtins
+  '("compose" "quote" "swap" "swap2" "swap3" "swapd" "swapt" "drop" "dsc" "len" "dip" "dip2" "dip3" "dup" "dup2" "dup3" "dup4" "dupd" "dupt" "dupq" "dsc2" "dsc3" "dscd" "dsct" "dscq" "over" "over2" "over3" "over4" "read"))
 
 (defvar stem-operators
-  '("+" "-" "*" "/" "<=" ">=" "!=" "=" ">" "<"))
+  '("+" "-" "*" "/" "<=" ">=" "!=" "=" ">" "<" "?" "."))
 
-(defvar stem-tab-width 4 "Width of a tab for stem mode")
+(defvar stem-typeops
+  '("type" "tostr" "stoi"))
+
+(defvar stem-tab-width 4 "Width of a tab for stem mode.")
 
 (defvar stem-font-lock-defaults
   `((
      ;; stuff between double quotes
      ("\"\\.\\*\\?" . font-lock-string-face)
-     ("[\\|]" . font-lock-bracket-face)
-     ( ,(regexp-opt stem-builtins 'words) . font-lock-builtin-face)
+     ( ,(regexp-opt stem-functions 'words) . font-lock-builtin-face)
+     ( ,(regexp-opt stem-stack-builtins 'words) . font-lock-builtin-face)
      ( ,(regexp-opt stem-constants 'words) . font-lock-constant-face)
-     ("[a-zA-Z_'0-9]+" . font-lock-function-name-face)
-     ( ,(regexp-opt stem-functions 'words) . font-lock-keyword-face)
-     ( ,(regexp-opt stem-operators 'words) . font-lock-operator-face)
-     ("\\([0--9]+\\.[0-9]+\\)" . font-lock-number-face))))
+     ;; ("[a-zA-Z_][a-zA-Z0-9]*" . font-lock-warning-face)
+     ( ,(regexp-opt stem-builtins 'words) . font-lock-keyword-face)
+     ( ,(regexp-opt stem-typeops 'words) . font-lock-type-face)
+     ( ,(regexp-opt stem-operators 'symbols) . font-lock-constant-face)
+     ("\\(\\b[0-9]+\\|\\)\\(\\.\\)\\([0-9]+\\(e[-]?[0-9]+\\)?\\([lL]?\\|[dD]?[fF]?\\)\\)\\b" . font-lock-string-face)
+     ("\\b\\(\\(0\\|[1-9][0-9]*\\)[uUlL]*\\)\\b" . font-lock-string-face))))
+
+
 
 
 (define-derived-mode stem-mode fundamental-mode "stem script"
-  "stem mode is a major mode for editing stem files"
-  ;; you again used quote when you had '((stem-hilite))
-  ;; I just updated the variable to have the proper nesting (as noted above)
-  ;; and use the value directly here
+  "Stem mode is a major mode for editing stem files."
   (setq font-lock-defaults stem-font-lock-defaults)
 
   (when stem-tab-width
@@ -47,3 +66,4 @@
 
 
 (provide 'stem-mode)
+;;; stem-mode.el ends here
